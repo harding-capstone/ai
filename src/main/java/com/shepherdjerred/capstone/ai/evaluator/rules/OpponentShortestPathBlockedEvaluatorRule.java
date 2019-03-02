39f8598cf2e24a2082ff49704326b8b784d040d1
Tre' Leday <aleday@harding.edu>
2019-03-02 17:15:03 -0600

    Added Evaluator classes and descriptions

---
+++ src/main/java/com/shepherdjerred/capstone/ai/evaluator/rules/OpponentShortestPathBlockedEvaluatorRule.java
@@ -0,0 +1,9 @@
+package com.shepherdjerred.capstone.ai.evaluator.rules;
+
+public class OpponentShortestPathBlockedEvaluatorRule {
+  /*
+  If our opponent(s)'s shortest path can be blocked with a single wall so that it no longer reaches
+  the goal state, good.
+   */
+
+}
---
+++ src/main/java/com/shepherdjerred/capstone/ai/evaluator/rules/ShortenActivePlayerPathEvaluatorRule.java
@@ -0,0 +1,10 @@
+package com.shepherdjerred.capstone.ai.evaluator.rules;
+
+public class ShortenActivePlayerPathEvaluatorRule {
+  /*
+  If the activePlayer has more than one path and can force a short one by placing a wall, that's
+  good. So check wall-ends and place a wall at one of them to shorten a path (have to check
+  horizontal and vertical).
+   */
+
+}
