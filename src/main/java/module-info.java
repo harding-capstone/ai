cfb4fadfc488bcd337fa0665162805fafef6d80d
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-03-12 22:58:55 -0500

    Skip javadoc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module com.shepherdjerred.capstone.ai {
   requires static lombok;
+
   requires com.shepherdjerred.capstone.logic;
   requires org.apache.logging.log4j;
   requires ai.algorithms;
   requires io.jenetics.base;
   requires com.google.common;
+
   exports com.shepherdjerred.capstone.ai;
   exports com.shepherdjerred.capstone.ai.alphabeta;
   exports com.shepherdjerred.capstone.ai.evaluator;
